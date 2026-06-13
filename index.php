db0915272eac25310da79a6f62ab88903ae81849
Rasel Ahmed <work.raseloffice@gmail.com>
2026-06-13 13:28:19 +0300

    deploy site to git

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Our website is coming soon</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"/>
+	<meta name="wb_main" content="wb_element 83bcdf7428ae4f9e786da8bc1e6df88335a61aa8c1922b74c01fa9c9aebace2b" />
+	<style type="text/css">
+		html {
+			min-height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+		body {
+			padding: 0;
+			margin: 0;
+			border: none;
+			font-size: 30px;
+			font-weight: normal;
+			font-family: Verdana,Arial,Helvetica,sans-serif;
+			line-height: 48px;
+			color: #FAF9F9;
+			text-align: center;
+			text-shadow: 2px 2px 2px rgba(0,0,0,0.3);
+			background-color: #DA5930;
+		}
+		.wb-content {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: center;
+			margin: 0 40px;
+		}
+		.wb-hr {
+			display: block;
+			border-top: 4px solid #FAF9F9;
+			width: 100%;
+			max-width: 300px;
+			margin: 30px auto;
+			box-shadow: 2px 2px 2px rgba(0,0,0,0.3);
+		}
+		h1 {
+			display: block;
+			font-size: 80px;
+			font-weight: normal;
+			font-family: Verdana,Arial,Helvetica,sans-serif;
+			line-height: 80px;
+			margin: 20px 0px 20px 0;
+			padding: 0;
+		}
+		div.wb-domain {
+			display: block;
+			text-decoration: underline;
+		}
+		.wb-list {
+			display: block;
+			text-align: left;
+			margin: 100px 0;
+		}
+		.wb-list ol {
+			padding: 10px 0;
+			margin: 0 0 0 42px;
+			list-style-position: outside;
+		}
+		.wb-powered {
+			display: block;
+			margin-top: 20px;
+			font-size: 23px;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+		}
+		.wb-powered img {
+			margin-left: 10px;
+		}
+		.wb-powered a {
+			margin-left: 8px;
+			color: #FAF9F9;
+			text-decoration: underline;
+		}
+		.wb-powered a:visited,
+		.wb-powered a:hover {
+			color: #FAF9F9;
+			text-decoration: underline;
+		}
+		.wb_main { display: none; }
+		@media (max-width: 991px) {
+			body {
+				font-size: 24px;
+				line-height: 38px;
+			}
+			h1 {
+				font-size: 64px;
+				line-height: 64px;
+			}
+			.wb-hr {
+				margin: 24px auto;
+			}
+			.wb-list {
+				margin: 80px 0;
+			}
+			.wb-list ol {
+				margin: 0 0 0 34px;
+			}
+			.wb-powered {
+				font-size: 18px;
+			}
+		}
+		@media (max-width: 767px) {
+			body {
+				font-size: 15px;
+				line-height: 24px;
+			}
+			h1 {
+				font-size: 40px;
+				line-height: 40px;
+			}
+			.wb-hr {
+				margin: 15px auto;
+			}
+			.wb-list {
+				margin: 50px 0;
+			}
+			.wb-list ol {
+				margin: 0 0 0 21px;
+			}
+			.wb-powered {
+				font-size: 12px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="wb-content">
+		<div class="wb-hr"></div>
+		<h1>Our website is coming soon</h1>
+		<div class="wb-domain">kosterferiwala.rf.gd</div>
+		<div class="wb-list">
+			<div><b>Manage Website:</b></div>
+			<ol>
+							<li>Login to your server Control Panel</li>
+							<li>Launch Website Builder</li>
+							<li>Choose Template. Edit Website</li>
+							<li>Publish</li>
+						</ol>
+		</div>
+		<div class="wb-hr"></div>
+		<div class="wb-powered">Powered by <a href="https://site.pro/?utm_source=b2b&amp;utm_medium=free&amp;utm_campaign=under_construction&amp;utm_content=kosterferiwala.rf.gd" title="Site.pro" target="_blank">Site.pro</a></div>
+	</div>
+</body>
+</html>
